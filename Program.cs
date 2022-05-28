06192204985f91e403c2cb7d063a08c75474c4c2
vrb9999 <55597012+vrb9999@users.noreply.github.com>
2022-05-28 09:01:21 +0530

    Prime factors

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace primeFactorisation
+{
+//    Factors
+//a.Desc ­> Computes the prime factorization of N using brute force.
+//b.I/P ­> Number to find the prime factors
+//c.Logic ­> Traverse till i* i <= N instead of i <= N for efficiency.
+//d.O/P ­> Print the prime factors of number N.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to get prime factorization : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i=1; i<=n; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.WriteLine("Prime factors :" + i);
+                }
+            }
+        }
+    }
+}
